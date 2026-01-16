e54f31aa2124810c0c7eff3784948d2d433db58c
Nathan Griffiths <ngriffiths@postmedia.com>
2026-01-15 19:01:55 -0800

    initial commit

--- src/App.svelte
+++ src/App.svelte
@@ -10,17 +10,15 @@
     import gfx from '/images/map.jpg';
 
     // SET CHART HEIGH & URL HERE...
-    const chartId01 = '5258140';
-    const chartId02 = '5258446';
+    const chartId01 = '27235207';
+    const chartId02 = '27235238';
     const chartHeight = '375px';
-    const fullWidthId = '25071549'
-    const fullWidthHeight = '300px';
 </script>
 
 <!-- MARKUP -->
 <header>
-    <h1>HED</h1>
-    <p class="subhead">TK TK TK</p>
+    <h1>Crimes going unreported</h1>
+    <p class="subhead">Many B.C. businesses are not reporting property and other non-violent crimes, according to a recent survey conducted by the Business Improvement Association of B.C.</p>
 </header>
 
 <!-- SIDE-BY-SIDE -->
@@ -29,47 +27,11 @@
     <iframe src='https://flo.uri.sh/visualisation/{chartId02}/embed' title='Interactive or visual content' frameborder='0' scrolling='no' style='width:100%;height:{chartHeight};' sandbox='allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation'></iframe>
 </div>
 
-<!-- FULL WIDTH -->
-<div class="graphic-container full-width">
-    <iframe src='https://flo.uri.sh/visualisation/{fullWidthId}/embed' title='Interactive or visual content' frameborder='0' scrolling='no' style='width:100%;height:{fullWidthHeight};' sandbox='allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation'></iframe>
-</div>
+ <p class="note">NOTE: The survey of 260 businesses was conducted in November 2025 and is not representational. No margin of error can be calculated.</p>
 
-<!-- IMAGES -->
- <div class="graphic-container">
-    <div class="img-container">
-        <img src={gfx} alt="data visualization"/>
-    </div>
- </div>
 
 <footer>
-    <p class="source">Source:
-        <a href="https://www.vancouversun.com/" target="_blank">tk</a>,
-        <a href="https://www.openstreetmap.org/" target="_blank">tk</a>
-    </p>
+    <p class="source">Source: Business Improvement Association of B.C.</p>
     <!-- flourish logo -->
     <div style='width:100%!;margin-top:4px!important;text-align:right!important;'><a class='flourish-credit' href='https://public.flourish.studio/visualisation/3706064/?utm_source=embed&utm_campaign=visualisation/3706064' target='_top' style='text-decoration:none!important'><img alt='Made with Flourish' src='https://public.flourish.studio/resources/made_with_flourish.svg' style='width:105px!important;height:16px!important;border:none!important;margin:0!important;'> </a></div>
 </footer>
-
-<!-- CSS -->
-<style>
-    /* STEPS */
-    section {
-        position: relative;
-    }
-    .sticky { 
-        align-items: center;
-        justify-content: center;
-        margin-bottom: 1.5rem;
-        position: sticky;
-        top: 5vh;
-        z-index: 1;
-    }
-    .chart .no-pointer {
-        pointer-events: none;
-    }
-    .steps {
-        margin-top: -100%;
-        position: relative;
-        z-index: 2;
-    }
-</style>
